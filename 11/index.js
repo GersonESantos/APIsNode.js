e4f89e2694b76a3337c60082ab65ec161ecd5032
GersonESS <gebhsantos@gmail.com>
2025-07-02 00:24:19 -0300

    a

--- 11/index.js
+++ 11/index.js
@@ -1,16 +1,22 @@
 // Importa o módulo Express para usar suas funcionalidades 
 const express = require("express");
+const path = require("path");
 
+const cors = require('cors');
 // Cria uma instância do aplicativo Express
 const app = express();
 
-app.use(express.json()); // Permite que o Express entenda JSON 
+app.use(cors()); // Permite requisições de outros domínios (CORS)
+app.use(express.json()); // Permite que o Express entenda JSON
+
+// Serve os arquivos estáticos da pasta 'frontend'
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Importa as rotas definidas em pessoa.js
 const pessoaRouter = require('./rotas/pessoa');
 
-// Usa o router definido para o caminho "/pessoa"
-app.use('/', pessoaRouter);
+// Usa o router definido para o caminho "/api" para não conflitar com o frontend
+app.use('/api', pessoaRouter);
 
 
 // Executa o projeto na porta especificada 
